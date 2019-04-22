5bbe000905645f42342ff88b70874a5ddea38766
Jiayi Wang <clairejw1015@gmail.com>
2019-04-22 23:44:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-var name = "Susan"
+var name = "Joe"
 var height = "74"
 
 
-if (name === "Susan"){
-  console.log("The name is correct!")
-} else {
-  console.log("expected the name to be Susan")
-}
+// if (name === "Susan"){
+//   console.log("The name is correct!")
+// } else {
+//   console.log("expected the name to be Susan")
+// }
 
 
 // Don't worry about this
